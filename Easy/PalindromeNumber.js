8f4808b77462ed790d89edc57baa98cb10a186db
tishanW98 <shakilatishan1998@outlook.com>
2024-07-18 10:52:11 +0530

    Palindrome Number Problem Solved

---
+++ Easy/PalindromeNumber.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  let str = x.toString();
+  let n = str.length;
+  let reversed = '';
+  for(let i=n-1; i>=0;i--){
+      reversed += str[i];
+  }
+  if(str ===  reversed){
+      return true
+  }else {
+      return false
+  }
+};
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
