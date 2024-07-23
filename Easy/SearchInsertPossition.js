d6c3d066330472baa9e4637cff358c176c83c2eb
tishanW98 <shakilatishan1998@outlook.com>
2024-07-23 23:31:00 +0530

    binary search solution added with O(log n)

--- Easy/SearchInsertPossition.js
+++ Easy/SearchInsertPossition.js
@@ -5,8 +5,34 @@ var searchInsert = function (nums, target) {
   }
   return i;
 };
+// Time Complexity : O(n)
+
+//--------------------------------------------//
+// Solution with Binary Search implementation //
+//--------------------------------------------//
+
+var searchInsertBinarySearch = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+};
+// Time Complexity : O(log n)
 
 const nums = [1, 3, 5, 6];
 const target = 5;
 
 console.log(searchInsert(nums, target));
+console.log(searchInsertBinarySearch(nums, target));
