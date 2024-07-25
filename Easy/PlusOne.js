8b5b8257df5da3a9bdbed7d8fec3878e0dbb3bda
tishanW98 <shakilatishan1998@outlook.com>
2024-07-25 15:55:06 +0530

    Plus one problem solved with unshift method

--- Easy/PlusOne.js
+++ Easy/PlusOne.js
@@ -36,7 +36,23 @@ var plusOne = function(digits) {
   return newDigits;
 };
 
+//----------------------------------------------------------------//
+
+var plusOneShort = function(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+          digits[i]++;
+          return digits;
+      }
+      digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
+};
+
 const digits1 = [1,2,3]
 const digits2 = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
+const digits3 = [9]
 console.log(plusOneSmall(digits1));
 console.log(plusOne(digits2));
+console.log(plusOneShort(digits3));
