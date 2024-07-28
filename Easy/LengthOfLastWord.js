ff5b20c339793fa2b13bb516d146a30e29085e7f
tishanW98 <shakilatishan1998@outlook.com>
2024-07-28 23:29:07 +0530

    easy solution for problem

--- Easy/LengthOfLastWord.js
+++ Easy/LengthOfLastWord.js
@@ -13,6 +13,19 @@ var lengthOfLastWord = function (s) {
   return k;
 };
 
+//-----------------------------------------------//
+//      Another solution to find the length      //
+//-----------------------------------------------//
+
+var lengthOfLastWordB = function(s) {
+  const words = s.trim().split(" ");
+  return words[words.length - 1].length;
+};
+
 console.log(lengthOfLastWord("Hello World")); 
 console.log(lengthOfLastWord("   fly me   to   the moon  "));
 console.log(lengthOfLastWord("luffy is still joyboy"));
+
+console.log(lengthOfLastWordB("Hello World")); 
+console.log(lengthOfLastWordB("   fly me   to   the moon  "));
+console.log(lengthOfLastWordB("luffy is still joyboy"));
